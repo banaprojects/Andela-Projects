54acfb578f17531861f40af9adabddf4c1b2cb5a
Christianib003 <christianib003@gmail.com>
2024-01-07 16:05:09 +0200

    Uploaded the snapchat codes

---
+++ SnapChat/login.js
@@ -0,0 +1,16 @@
+// const button = document.querySelector('#login');
+// const validateUname = document.getElementById('input1');
+// const validatePwd = document.getElementById('input2');
+
+// function validateInputs() {
+//     if (validateUname.value.trim() === '' || validatePwd.value.trim() === '') {
+//         button.style.backgroundColor = '#BBB';
+//         button.style.color = '#FFF';
+//     } else {
+//         button.style.backgroundColor = '#FFFC00';
+//         button.style.color = '#000';
+//     }
+// }
+
+// validateUname.addEventListener('input', validateInputs);
+// validatePwd.addEventListener('input', validateInputs);
